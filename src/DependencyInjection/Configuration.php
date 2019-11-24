ca890529c5cdc5fcb987f570273b7af3ab0e7f1b
Jeroen van den Enden <info@endroid.nl>
2019-11-24 20:21:37 +0000

    Support multiple versions

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,39 +19,40 @@ final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('endroid_cm_sms');
 
-        $treeBuilder
-            ->root('endroid_cm_sms')
-                ->children()
-                    ->booleanNode('disable_delivery')->defaultFalse()->end()
-                    ->arrayNode('delivery_phone_numbers')
-                        ->prototype('scalar')->end()
-                    ->end()
-                    ->scalarNode('product_token')->isRequired()->end()
-                    ->arrayNode('defaults')
-                        ->children()
-                            ->scalarNode('unicode')
-                                ->validate()
-                                ->ifNotInArray(Client::getUnicodeOptions())
-                                    ->thenInvalid('Invalid unicode mode %s, choose one from ["'.implode('", "', Client::getUnicodeOptions()).'"]')
-                                ->end()
-                            ->end()
-                            ->scalarNode('sender')
-                                ->validate()
-                                ->always(function ($sender) {
-                                    Client::assertValidSender($sender);
+        if (method_exists($treeBuilder, 'root')) {
+            $rootNode = $treeBuilder->root('endroid_cm_sms');
+        } else {
+            $rootNode = $treeBuilder->getRootNode();
+        }
 
-                                    return $sender;
-                                })
-                                ->end()
-                            ->end()
-                            ->integerNode('minimum_number_of_message_parts')->min(1)->end()
-                            ->integerNode('maximum_number_of_message_parts')->end()
-                        ->end()
-                    ->end()
+        $rootNode
+            ->children()
+                ->booleanNode('disable_delivery')->defaultFalse()->end()
+                ->arrayNode('delivery_phone_numbers')
+                    ->prototype('scalar')->end()
                 ->end()
-            ->end()
+                ->scalarNode('product_token')->isRequired()->end()
+                ->arrayNode('defaults')
+                    ->children()
+                        ->scalarNode('unicode')
+                            ->validate()
+                            ->ifNotInArray(Client::getUnicodeOptions())
+                                ->thenInvalid('Invalid unicode mode %s, choose one from ["'.implode('", "', Client::getUnicodeOptions()).'"]')
+                            ->end()
+                        ->end()
+                        ->scalarNode('sender')
+                            ->validate()
+                            ->always(function ($sender) {
+                                Client::assertValidSender($sender);
+
+                                return $sender;
+                            })
+                            ->end()
+                        ->end()
+                        ->integerNode('minimum_number_of_message_parts')->min(1)->end()
+                        ->integerNode('maximum_number_of_message_parts')->end()
         ;
 
         return $treeBuilder;
